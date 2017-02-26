1328fd9db991a26766297b569b5f8901efeead12
METO <i@i-meto.com>
2017-02-26 15:45:33 +0800

    使用 multi_curl 做并发处理，支持多用户

--- bilibili/onlineHeart.php
+++ bilibili/onlineHeart.php
@@ -1,55 +1,102 @@
 <?php
 /**
  *  Author： METO
- *  Version: 0.1.0
+ *  Version: 0.2.0
  */
-
 class bilibili{
 
-    protected $_COOKIE='YOUR COOKIE';
-    protected $_USERAGENT='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.101 Safari/537.36';
-    protected $_REFERER='http://live.bilibili.com/';
+	public $user=array(
+		array(
+			'cookie' => '填写用户cookie',
+			'status' => 1,
+		),
+		array(
+			'cookie' => '第二个用户cookie',
+			'status' => 0, // 0 表示禁用
+		),
+        // 多用户以此类推
+    );
 
-    private function getinfo(){
-        $data=json_decode(self::curl('http://live.bilibili.com/User/getUserInfo'),1);
-        $a=$data['data']['user_intimacy'];
-        $b=$data['data']['user_next_intimacy'];
-        $per=round($a/$b*100,2);
-        echo "===============================\n";
-        echo "name: {$data['data']['uname']} \n";
-        echo "level: {$data['data']['user_level']} \n";
-        echo "exp: {$a}/{$b} {$per}%\n";
-        echo "sign: ".self::sign()."\n";
-        echo "===============================\n";
-    }
-
-    private function sign(){
-        $raw=json_decode(self::curl('http://live.bilibili.com/sign/doSign'),1);
-        return $raw['msg'];
-    }
+	public function display(){
+		header('Content-Type: text/txt; charset=UTF-8');
+		echo "===============================\n";
+		foreach($this->user as $result){
+			if($result['status']){
+				$data=$result['data'];
+				$a=$data['data']['user_intimacy'];
+		        $b=$data['data']['user_next_intimacy'];
+		        $per=round($a/$b*100,2);
+		        echo "name   : {$data['data']['uname']} \n";
+		        echo "level  : {$data['data']['user_level']} \n";
+		        echo "exp    : {$a}/{$b} [{$per}%]\n";
+				echo "status : {$result['cron']['msg']}\n";
+		        echo "===============================\n";
+			}
+			else{
+				echo "status : {$result['data']['msg']}\n";
+		        echo "===============================\n";
+			}
+		}
+	}
 
     public function cron(){
-        header('Content-Type: text/txt; charset=UTF-8');
-        echo date('[Y-m-d H:i:s]',time())."\n";
-        $raw=json_decode(self::curl('http://live.bilibili.com/User/userOnlineHeart'),1);
-        if(!isset($raw['data'][1]))echo " > SUCCESS\n";
-        else echo " > INFO: already send @ ".date('Y-m-d H:i:s',$raw['data'][1])."\n";
-
-        self::getinfo();
+		$mh=curl_multi_init();
+		foreach($this->user as $id=>$user){
+			if($user['status']!=1)continue;
+			$curl[$id]=$this->create('http://live.bilibili.com/User/userOnlineHeart',$user['cookie']);
+			curl_multi_add_handle($mh,$curl[$id]);
+		}
+		do{
+		    curl_multi_exec($mh,$running);
+		    curl_multi_select($mh);
+		}while($running>0);
+		foreach($curl as $id=>$c){
+			$result[$id]=curl_multi_getcontent($c);
+			curl_multi_remove_handle($mh,$c);
+		}
+		curl_multi_close($mh);
+		foreach($result as $id=>$vo){
+			$vo=json_decode($vo,1);
+			$this->user[$id]['cron']=$vo;
+		}
     }
 
-    protected function curl($url){
-        $curl=curl_init();
+	public function check(){
+		$mh=curl_multi_init();
+		foreach($this->user as $id=>$user){
+			if($user['status']!=1)continue;
+			$curl[$id]=$this->create('http://live.bilibili.com/User/getUserInfo',$user['cookie']);
+			curl_multi_add_handle($mh,$curl[$id]);
+		}
+		do{
+		    curl_multi_exec($mh,$running);
+		    curl_multi_select($mh);
+		}while($running>0);
+		foreach($curl as $id=>$c){
+			$result[$id]=curl_multi_getcontent($c);
+			curl_multi_remove_handle($mh,$c);
+		}
+		curl_multi_close($mh);
+		foreach($result as $id=>$vo){
+			$vo=json_decode($vo,1);
+			if($vo['code']<0)$this->user[$id]['status']=0;
+			$this->user[$id]['data']=$vo;
+		}
+	}
+
+	private function create($url,$cookie){
+		$curl=curl_init();
         curl_setopt($curl,CURLOPT_URL,$url);
+		curl_setopt($curl,CURLOPT_COOKIE,$cookie);
         curl_setopt($curl,CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl,CURLOPT_CONNECTTIMEOUT, 10);
-        curl_setopt($curl,CURLOPT_REFERER,$this->_REFERER);
-        curl_setopt($curl,CURLOPT_COOKIE,$this->_COOKIE);
-        curl_setopt($curl,CURLOPT_USERAGENT,$this->_USERAGENT);
-        $result=curl_exec($curl);
-        curl_close($curl);
-        return $result;
-    }
+        curl_setopt($curl,CURLOPT_REFERER,'http://live.bilibili.com/');
+        curl_setopt($curl,CURLOPT_USERAGENT,'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.56 Safari/536.5');
+		return $curl;
+	}
 }
 
-(new bilibili)->cron();
+$API=new bilibili;
+$API->check();
+$API->cron();
+$API->display();
