f39ed96064eb7f68142adda76e2bc274f2cb198a
metowolf <i@i-meto.com>
2018-02-11 14:28:33 +0800

    Update neteasemusic

--- neteasemusic/README.md
+++ neteasemusic/README.md
@@ -4,13 +4,9 @@
 
 ## 使用指南
 
-下载本目录下的所有文件，然后修改 `run.php` 中的两项配置：
-```
-$id = 14721111; //想要操作的歌单编号
-$cookie = ''; // 填入网易云音乐的 cookie 以便更精准判断是否变灰
-```
+下载本目录下的所有文件，直接执行 `php run.php`，没错就是这么简单。
 
-执行 `php run.php`，没错就是这么简单。
+下载完成后可以在 `download` 文件夹下找到所有歌曲文件，自由发挥
 
 注意这是在终端执行，那些扔到 PHP 虚拟主机后直接访问网页的不要来问我为什么不能用好吗～
 
@@ -21,19 +17,30 @@ $cookie = ''; // 填入网易云音乐的 cookie 以便更精准判断是否变�
 ## 效果
 ```
 $ php run.php
-找到 22 首灰色歌曲呢
+请输入网易云音乐歌单编号: 14721111
+请输入您的网易云音乐 cookie [可选]:
+啊，找到 21 首灰色歌曲呢
 
-正在点亮第 1 首歌曲《回忆那么伤 》- 孙子涵/双子涵
-匹配到《回忆那么伤》- 孙子涵/回忆那么伤，相似度 71%
+正在点亮第 1 首歌曲《火锅底料》- GAI爷/火锅底料
+ + check tencent
+ + + 匹配到《火锅底料》- GAI/火锅底料，相似度 94%
+ + check xiami
+ + check kugou
+ + check baidu
   % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                  Dload  Upload   Total   Spent    Left  Speed
-100 9239k  100 9239k    0     0  2720k      0  0:00:03  0:00:03 --:--:-- 2720k
+100 7375k  100 7375k    0     0  2112k      0  0:00:03  0:00:03 --:--:-- 2112k
 
 正在点亮第 2 首歌曲《孤儿仔》- 陈奕迅/Eason 4 A Change & Hits
-匹配到《孤儿仔》- 陈奕迅/Eason 4 A Chance & Hits，相似度 97%
+ + check tencent
+ + + 匹配到《孤儿仔》- 陈奕迅/Eason 4 A Chance & Hits，相似度 97%
+ + check xiami
+ + + 匹配到《孤儿仔》- 陈奕迅/Eason 4 A Change & Hits，相似度 100%
+ + check kugou
+ + check baidu
   % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                  Dload  Upload   Total   Spent    Left  Speed
-100 8715k  100 8715k    0     0  2653k      0  0:00:03  0:00:03 --:--:-- 2653k
+100 3475k  100 3475k    0     0  1601k      0  0:00:02  0:00:02 --:--:-- 1602k
 
 ...
 ```
--- neteasemusic/run.php
+++ neteasemusic/run.php
@@ -1,17 +1,33 @@
 <?php
+/**
+ * @version 0.2.1
+ * @author https://i-meto.com/
+ */
+
 require 'meting.php';
 use Metowolf\Meting;
 
-$id = 14721111;
-$cookie = '';
+$stdin = fopen('php://stdin', 'r');
+
+printf("请输入网易云音乐歌单编号: ");
+$str = fread($stdin, 1000);
+$id = $str;
+
+printf("请输入您的网易云音乐 cookie [可选]: ");
+$str = fread($stdin, 10000);
+$cookie = $str;
 
 $api = new Meting('netease');
-if(!empty($cookie))$api->cookie($cookie);
+if(!empty(trim($cookie))){
+    $api->cookie(trim($cookie));
+}
+else $api->cookie('os=pc; osver=Microsoft-Windows-10-Professional-build-10586-64bit; appver=2.0.3.131777; channel=netease; __remember_me=true');
+
 $data = $api->playlist($id);
-$data = json_decode($data,true);
+$data = json_decode($data, true);
 $jobs = array();
 foreach($data['privileges'] as $key=>$vo){
-    if($vo['st']!=0){
+    if($vo['st']!=0) {
         $tmp = $data['playlist']['tracks'][$key];
         $jobs[]=array(
             'id'        => $tmp['id'],
@@ -24,30 +40,38 @@ foreach($data['privileges'] as $key=>$vo){
 
 printf("啊，找到 %d 首灰色歌曲呢\n",count($jobs));
 
-$api = new Meting('tencent');
+$api = new Meting();
 foreach($jobs as $index=>$job){
     printf("\n正在点亮第 %d 首歌曲《%s》- %s/%s\n",$index+1,$job['name'],$job['artist'],$job['album']);
-    $data = $api->format(true)->search($job['name'].' '.$job['artist']);
-    $data = json_decode($data,true);
     $best = 0;
-    foreach($data as $vo){
-        $ta = $job['name'];
-        $tb = $vo['name'];
-        similar_text($ta,$tb,$per);
-        if($per < 50)continue;
-        $ta = $job['name'].$job['album'].$job['artist'];
-        $tb = $vo['name'].$vo['album'].$vo['artist'][0];
-        similar_text($ta,$tb,$per);
-        if($per > $best){
-            $best = $per;
-            $ans = $vo;
+    foreach(array('tencent', 'xiami', 'kugou', 'baidu') as $server) {
+        printf(" + check " . $server . "\n");
+        $api->site($server);
+        $data = $api->format(true)->search($job['name'].' '.$job['artist']);
+        $data = json_decode($data, true);
+        foreach($data as $vo){
+            $ta = $job['name'];
+            $tb = $vo['name'];
+            similar_text($ta,$tb,$per);
+            if($per < 60)continue;
+            $ta = $job['name'].$job['album'].$job['artist'];
+            $tb = $vo['name'].$vo['album'].$vo['artist'][0];
+            similar_text($ta,$tb,$per);
+            if ($per > $best) {
+                $url = $api->url($vo['url_id'], 320);
+                $url = json_decode($url, true);
+                if (!empty($url['url'])) {
+                    $best = $per;
+                    $ans = $vo;
+                    printf(" + + 匹配到《%s》- %s/%s，相似度 %d%%\n", $ans['name'], $ans['artist'][0], $ans['album'], $best);
+                }
+            }
         }
     }
-    printf("匹配到《%s》- %s/%s，相似度 %d%%\n",$ans['name'],$ans['artist'][0],$ans['album'],$best);
-
-    $url = $api->url($ans['url_id']);
-    $url = json_decode($url,true);
-    exec('curl -o download/'.$ans['id'].'.mp3 "'.$url['url'].'"');
+    $api->site($ans['source']);
+    $url = $api->url($ans['url_id'], 320);
+    $url = json_decode($url, true);
+    exec(sprintf("curl -o \"download/%s - %s.mp3\" \"%s\"", $ans['name'], implode(',', $ans['artist']), $url['url']));
 
     sleep(2);
 }
